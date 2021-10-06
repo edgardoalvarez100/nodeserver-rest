b287d9620e3c14710a9d8ec75e68bc23427070cb
Edgardo Alvarez <edgardo@livedesign.org>
2021-10-06 07:40:26 -0500

    agregado validation

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,10 +1,11 @@
 const { Router } = require("express");
+const { check } = require("express-validator");
 const { usuariosGet, usuariosPost, usuariosPut, usuariosDelete, usuariosPatch } = require("../controllers/usuarios");
 
 const router = Router();
 
 router.get('/',usuariosGet);
-router.post('/',usuariosPost);
+router.post('/',[check('correo','El correo no es valido').isEmail],usuariosPost);
 router.put('/:id',usuariosPut);
 router.delete('/:id',usuariosDelete);
 router.patch('/',usuariosPatch);
