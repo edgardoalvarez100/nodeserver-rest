90a8230d2cdede2993258df4a54039386204d1f8
Edgardo Alvarez <edgardoalvarez100@gmail.com>
2021-10-10 17:56:01 -0500

    modificado

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -3,7 +3,6 @@ const path = require("path")
 
  const cargarArchivo = (req,res = response) => {
 
-
   if (!req.files || Object.keys(req.files).length === 0 || !req.files.archivo) {
     res.status(400).json({msg: 'No hay archivos para subir'});
     return;
